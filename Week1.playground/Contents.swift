20d670f9d681f581eebcc27c9d92de9bb841f40c
bers <muzafferkonal@hotmail.com>
2022-05-20 09:40:56 +0300

    Initial Commit

---
+++ Week1.playground/Contents.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+class Person {
+    var name: String
+    var surname: String
+    var age: Int
+    var type: PersonType
+    var gender: Gender?
+    var toy: Toy?
+    
+    init(name: String, surname: String, age: Int, type: PersonType) {
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.type = type
+    }
+    
+}
+
+enum PersonType {
+    case kid
+    case grown
+}
+enum Gender {
+    case male
+    case female
+}
+
+struct Toy {
+    var name: String
+    var type: ToyType
+}
+enum ToyType {
+    case car
+    case doll
+}
+
+
